34a4021d75ced44e3881c3a021bd99c49420011a
Henok Berhanu Tsegaye <henokberhanut@gmail.com>
2024-07-27 21:36:04 -0600

    updates

---
+++ traffic.sh
@@ -0,0 +1,7 @@
+#################### Collect CPU and memory consumption for gNB-DU ####################################
+
+./enter_container.sh rfsim5g-oai-nr-ue
+./enter_container.sh rfsim5g-oai-du
+
+inside the rfsim5g-oai-du container: iperf3 -s
+inside the rfsim5g-oai-nr-ue container:  iperf3 -u -c 192.168.0.15 -b 128k -t 3600
